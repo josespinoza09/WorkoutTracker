27762401a9cab6f9ba07622d42082da68f0dd8b0
Jose Espinoza <jose20019004@gmail.com>
2021-03-24 20:34:51 -0400

    added the server with starter code

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const db = require('./models/');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
