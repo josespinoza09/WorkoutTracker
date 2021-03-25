d393d11801c3d6af7c0161217f548b0d5fa70bda
Jose Espinoza <jose20019004@gmail.com>
2021-03-24 20:35:36 -0400

    connection to mongo

--- server.js
+++ server.js
@@ -10,3 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/Fitness',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
